f6738bd8712f2c293a92e95be2f07be49e4c4b39
mallow7 <sneadbc78@gmail.com>
2026-02-15 14:44:54 -0500

    Update vc-role-bot.js

--- vc-role-bot.js
+++ vc-role-bot.js
@@ -1,7 +1,7 @@
 const { Client, GatewayIntentBits } = require('discord.js');
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 
 if (!process.env.BOT_TOKEN) {
